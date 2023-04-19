1bec5995e11af688fdeebc7a6332b91491330de4
baexxbin <soo3886@gmail.com>
2023-04-19 09:35:05 +0900

    feat(MemberController): 회원목록 컨트롤러 작성

--- src/main/java/jpabucket/bucketlist/Controller/MemberController.java
+++ src/main/java/jpabucket/bucketlist/Controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,7 +22,7 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
-        return "members/createMemberform";
+        return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
@@ -39,5 +40,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
