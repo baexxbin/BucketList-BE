c1cbc54fa15c6d5a64484241ac31f96b3517bf05
baexxbin <soo3886@gmail.com>
2023-06-06 02:17:01 +0900

    feat: ToDo 생성매소드 작성
    Item 추상클래스의 자식매소드에 생성매소드 작성

--- src/main/java/jpabucket/bucketlist/domain/item/ToDo.java
+++ src/main/java/jpabucket/bucketlist/domain/item/ToDo.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 @DiscriminatorValue("D")
 @Getter
@@ -16,7 +19,18 @@ import lombok.Setter;
 public class ToDo extends Item {
 	private String way;
 
-	public void editTodo(String goal, String way, String targetDate) {
+	//==생성 매소드==//
+	public static ToDo createToDo(String goal, LocalDate targetDate, String way) {
+		ToDo toDo = new ToDo();
+		toDo.setGoal(goal);
+		toDo.setTargetDate(targetDate);
+		toDo.setHeart(0);
+		toDo.setWay(way);
+
+		return toDo;
+	}
+
+	public void editTodo(String goal, String way, LocalDate targetDate) {
 		this.setGoal(goal);
 		this.setWay(way);
 		this.setTargetDate(targetDate);
