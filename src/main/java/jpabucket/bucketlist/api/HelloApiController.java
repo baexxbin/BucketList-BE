70bcaef4c7cd62def0df12ab21137fdca162b8bd
baexxbin <soo3886@gmail.com>
2023-06-10 19:43:25 +0900

    feat: BucketList-FE(Vue.js)와 연동

---
+++ src/main/java/jpabucket/bucketlist/api/HelloApiController.java
@@ -0,0 +1,15 @@
+package jpabucket.bucketlist.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloApiController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "hello!";
+    }
+}
