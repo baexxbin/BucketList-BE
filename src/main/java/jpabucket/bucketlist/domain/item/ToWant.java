eb0829e5c331417eea2f450630beede3bd1644c4
baexxbin <soo3886@gmail.com>
2023-07-17 15:21:19 +0900

    feat: ToWant 생성매소드 작성

--- src/main/java/jpabucket/bucketlist/domain/item/ToWant.java
+++ src/main/java/jpabucket/bucketlist/domain/item/ToWant.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Entity
 @DiscriminatorValue("W")
 @Getter
@@ -16,4 +18,23 @@ import lombok.Setter;
 public class ToWant extends Item {
 	private String reason;
 	private int price;
+
+	//==생성 매소드==//
+	public static ToWant createToWant(String goal, LocalDate targetDate, String reason, int price) {
+		ToWant toWant = new ToWant();
+		toWant.setGoal(goal);
+		toWant.setTargetDate(targetDate);
+		toWant.setReason(reason);
+		toWant.setPrice(price);
+		toWant.setHeart(0);
+
+		return toWant;
+	}
+
+	public void editToWant(String goal, LocalDate targetDate, String reason, int price) {
+		this.setGoal(goal);
+		this.setTargetDate(targetDate);
+		this.setReason(reason);
+		this.setPrice(price);
+	}
 }
