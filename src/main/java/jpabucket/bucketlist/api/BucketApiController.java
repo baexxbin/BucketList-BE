eb241bbbae7b3781fd4b9e46da769e6b847f9f69
baexxbin <soo3886@gmail.com>
2023-07-18 14:13:11 +0900

    feat: 버킷 등록 API BucketApiController 작성

---
+++ src/main/java/jpabucket/bucketlist/api/BucketApiController.java
@@ -0,0 +1,32 @@
+package jpabucket.bucketlist.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jpabucket.bucketlist.dto.bucket.BucketRequestDto;
+import jpabucket.bucketlist.dto.bucket.BucketResponseDto;
+import jpabucket.bucketlist.dto.bucket.ItemDto;
+import jpabucket.bucketlist.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequiredArgsConstructor
+public class BucketApiController {
+
+    private final ItemService itemService;
+
+    @Operation(summary = "버킷 생성", description = "Return BucketId")
+    @PostMapping("/api/v1/create")
+    public ResponseEntity<BucketResponseDto> create(@RequestBody @Valid BucketRequestDto requestDto) {
+        ItemDto itemDto = requestDto.toServicItemDto();
+        Long bucketId = itemService.createBucket(itemDto);
+        BucketResponseDto responseDto = new BucketResponseDto(bucketId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+}
