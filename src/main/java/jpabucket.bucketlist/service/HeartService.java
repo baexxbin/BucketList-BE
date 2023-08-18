426989fe3d7afcab6bed352ba9665fa075956aca
baexxbin <soo3886@gmail.com>
2023-08-18 12:07:52 +0900

    refactor(HeartService): 하트 추가, 하트 취소 매소드 분리

--- src/main/java/jpabucket.bucketlist/service/HeartService.java
+++ src/main/java/jpabucket.bucketlist/service/HeartService.java
@@ -3,35 +3,62 @@ package jpabucket.bucketlist.service;
 import jpabucket.bucketlist.domain.Heart;
 import jpabucket.bucketlist.domain.Member;
 import jpabucket.bucketlist.domain.item.Item;
+import jpabucket.bucketlist.dto.bucket.BaseBucketDto;
+import jpabucket.bucketlist.exceptions.CustomException;
 import jpabucket.bucketlist.repository.HeartRepository;
 import jpabucket.bucketlist.repository.ItemRepository;
+import jpabucket.bucketlist.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class HeartService {
-
     private final HeartRepository heartRepository;
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+
+
 
     @Transactional
-    public boolean pressHeart(Member member, Long itemId) {
+    public void pressHeart(BaseBucketDto baseBucketDto) throws IOException {
+        Long memberId = baseBucketDto.getMemberId();
+        Long itemId = baseBucketDto.getBucketId();
+
+        Member member = memberRepository.findOne(memberId);     // 리펙토리 예정: 로그인한 사용자 정보 가져오기
         Item item = itemRepository.findOne(itemId);
+
         Optional<Heart> heartOptional = heartRepository.findByMemberAndItem(member, item);
 
-        if (heartOptional.isPresent()) {    // 이미 하트를 누른 경우 하트 취소
-//            Heart existingHeart = heartOptional.get();
-            heartRepository.deleteByMemberAndItem(member, item);
-            item.decreaseHeart();
-            return false;
+        if (heartOptional.isPresent()) {    // 이미 하트를 누른 경우
+            throw new CustomException("ALREADY_HEARTED");
         }
-        Heart heart = Heart.createHeart(member, item);  // 새롭게 하트 누를 경우
+
+        Heart heart = Heart.createHeart(member, item);
         heartRepository.save(heart);
         item.addHeart();
-        return true;
+    }
+
+    @Transactional
+    public void unPressHeart(BaseBucketDto baseBucketDto) throws IOException {
+
+        Long memberId = baseBucketDto.getMemberId();
+        Long itemId = baseBucketDto.getBucketId();
+
+        Member member = memberRepository.findOne(memberId);     // 리펙토리 예정: 로그인한 사용자 정보 가져오기
+        Item item = itemRepository.findOne(itemId);
+
+        Optional<Heart> heartOptional = heartRepository.findByMemberAndItem(member, item);
+
+        if (heartOptional.isEmpty()) {
+            throw new CustomException("HEART_NOT_FOUND");
+        }
+
+        heartRepository.deleteByMemberAndItem(member, item);
+        item.decreaseHeart();
     }
 }
